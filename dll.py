ac7f3704a36e92592b5aa135adbb9865b5273b9d
mikaeleythor <eythor108@gmail.com>
2021-02-22 14:26:26 +0000

    relink function added

--- dll.py
+++ dll.py
@@ -22,19 +22,17 @@ class DLL:
 
     def relink(self, current, predecessor, successor):
         """Link new node"""
-
-        current.next = successor                            # Links from new node
-        if current != predecessor:
-            current.prev = predecessor                          # -------------------
-
-        try:                                                    # Links to new node
-            predecessor.next = current
-        except AttributeError:
-            self.header.next = current
+        current.next = successor # sama hvað current er, gildir alltaf (current er aldrei header)
         try:
-            successor.prev = current
+            successor.prev = current # ef nonetype error kemur þá er successor = trailer
         except AttributeError:
-            self.trailer.prev = current                         # -------------------
+            self.trailer.prev = current # búinn að tengja current við successor og öfuct fyrir delink og relink
+        if current != predecessor:
+            current.prev = predecessor
+            try:
+                predecessor.next = current
+            except AttributeError:
+                self.header.next = current
 
     def insert(self, data):
         successor = self.get_current_node(self.position)
@@ -47,18 +45,11 @@ class DLL:
 
     def remove(self):
         if self.size > 0:
-            # removed = self.header.next
-            # successor = removed.next
-            # successor.prev = self.header
-            # self.header.next = successor
-            # self.size -= 1
-            # if self.position >= self.size:
-            #     self.position -= 1
             removed = self.get_current_node(self.position)
-            successor = removed.next
-            predecessor = removed.prev
-            self.relink(predecessor, predecessor, successor)
-
+            self.relink(removed.prev, removed.prev, removed.next)
+            self.size -= 1
+            if self.position > self.size and self.position > 1:
+                self.position -= 1
             data = removed.data                                 # Record data deletion
             removed.data = removed.prev = removed.next = None   # Deprecate node
             return data                                         # Return deleted data
@@ -66,9 +57,6 @@ class DLL:
             pass
 
     def get_value(self):
-        # node = self.header.next
-        # for _ in range(self.position-1):
-        #     node = node.next
         node = self.get_current_node(self.position)
         return node.data
 
@@ -110,7 +98,8 @@ class DLL:
         temp_var = self.header.next
         for _ in range(self.size):
             ret_list[_] = temp_var.data
-            temp_var = temp_var.next
+            if self.size > 1:
+                temp_var = temp_var.next
         return ' '.join([str(value) for value in ret_list])
 
 if __name__ == "__main__":
@@ -133,4 +122,10 @@ if __name__ == "__main__":
     print(str(dll) + "   -   current value: " + str(dll.get_value()) + "   -   size: " + str(len(dll)))
     # print(dll.position)
     dll.insert("1")
+    print(str(dll) + "   -   current value: " + str(dll.get_value()) + "   -   size: " + str(len(dll)))
+    dll.remove()
+    print(str(dll) + "   -   current value: " + str(dll.get_value()) + "   -   size: " + str(len(dll)))
+    dll.remove()
+    print(str(dll) + "   -   current value: " + str(dll.get_value()) + "   -   size: " + str(len(dll)))
+    dll.move_to_pos(2)
     print(str(dll) + "   -   current value: " + str(dll.get_value()) + "   -   size: " + str(len(dll)))
